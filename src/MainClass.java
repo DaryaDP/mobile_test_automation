26d63d3325798c0d3248af86913a39f8aa09b995
DaryaDP <d.piterimova@gmail.com>
2019-08-11 22:05:19 +0300

    Lesson 0 Task 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,14 @@
 public class MainClass {
 
+    private int class_number = 20;
+
+
     public int getLocalNumber() {
         int a = 14;
         return a;
+    }
+
+    public int getClassNumber() {
+        return class_number;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
 public class MainClassTest {
+
+//lesson 0 task 1
     @Test
     public void testGetLocalNumber() {
         MainClass newObject = new MainClass();
@@ -8,6 +10,18 @@ public class MainClassTest {
             System.out.println("Local number is 14");
         } else {
             System.out.println("Error! Local number is not 14 :(");
+        }
+    }
+
+//lesson 0 task 2
+    @Test
+    public void testGetClassNumber() {
+        MainClass newObject = new MainClass();
+        if (newObject.getClassNumber() > 45) {
+            System.out.println("Class Number more than 45");
+        } else {
+            System.out.println("Class Number is 45 or less than 45");
+
         }
     }
 }
